56b0ee9cb3863af6a61ae5231d9406bdc2206fdf
cygcyg <satoshi_ohkawa@oizu.co.jp>
2018-06-25 23:34:30 +0900

    課題要件達成

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NameArea: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        resultViewController.name = NameArea.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
 
     override func didReceiveMemoryWarning() {
